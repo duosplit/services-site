6d07ea79283bad20072c1563cc1587ec430925a5
Mathieu Provencher <3145731+matprov@users.noreply.github.com>
2023-02-06 22:25:29 -0500

    send email

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 // FORM
 function sendForm() {
+    var destinationEmail = "contact@solutionslogiciellesmp.com";
     var name = $('#form-name').val();
     var email = $('#form-email').val();
     var subject = $('#form-subject').val();
-    var message = $('#form-message').val();
+    var message = $('#form-message').val().replace(/\n/g, '%0A');
 
-    var emailString = "mailto:" + email + "?subject=" + subject + "&body=" + message + "%0A%0A" + name;
+    var emailString = "mailto:" + destinationEmail + "?subject=" + subject + "&body=" + message + "%0A%0A" + name + "%0A" + email;
 
     window.open(emailString);
 }
